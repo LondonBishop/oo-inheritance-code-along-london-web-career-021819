bd3f33a10eed8ba908a75ad0c65e1ec48252ee1c
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-21 18:14:41 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -2,10 +2,17 @@ class Vehicle
   
   attr_accessible :wheel_size, :wheel_number
 
-  def initialize(wheelsize, wheelnumber)
+  def initialize(wheelsize, number)
     @wheel_size = wheelsize
     @wheel_number = number
   end
 
+  def wheel_size
+      @wheel_size 
+  end
+  
+  def wheel_number
+     @wheel_number
+  end
 
 end
